9ff4357cd703b666df7cb1e43964d690540d8937
vicentemsduoc <vice.garciam@duocuc.cl>
2026-07-18 19:36:58 -0400

    Update InicioController.java

--- src/main/java/com/usuarios/UsuariosRest/controllers/InicioController.java
+++ src/main/java/com/usuarios/UsuariosRest/controllers/InicioController.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
 public class InicioController {
 
 
-    private String version = "1.2";
+    private String version = "1.3";
     @GetMapping("/")
     public String comienzo(){
         Calendar calendario = Calendar.getInstance();
